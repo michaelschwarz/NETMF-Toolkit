e1f11ece0ee9605fcac36b57462d18b5e0cb7b4f
interactive_cp <SND\interactive_cp>
2009-03-09 16:03:47 +0000

    added NtpClient

---
+++ NTP/NtpClient.cs
@@ -0,0 +1,93 @@
+﻿/* 
+ * NtpClient.cs
+ * 
+ * Copyright (c) 2009, Michael Schwarz (http://www.schwarz-interactive.de)
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
+ * ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
+ * CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * 
+ * MS   09-02-16    added NtpClient
+ * 
+ * 
+ */
+using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace MSchwarz.Net.Ntp
+{
+    public class NtpClient
+    {
+        public static DateTime GetNetworkTime()
+        {
+            return GetNetworkTime("time-a.nist.gov");
+        }
+
+        public static DateTime GetNetworkTime(string ntpServer)
+        {
+            IPAddress[] address = Dns.GetHostEntry(ntpServer).AddressList;
+
+            if(address == null || address.Length == 0)
+                throw new ArgumentException("Could not resolve ip address from '" + ntpServer + "'.", "ntpServer");
+
+            IPEndPoint ep = new IPEndPoint(address[0], 123);
+
+            return GetNetworkTime(ep);
+        }
+
+        public static DateTime GetNetworkTime(IPEndPoint ep)
+        {
+            Socket s = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+
+            s.Connect(ep);
+
+            byte[] ntpData = new byte[48]; // RFC 2030 
+            ntpData[0] = 0x1B;
+            for (int i = 1; i < 48; i++)
+                ntpData[i] = 0;
+
+            s.Send(ntpData);
+            s.Receive(ntpData);
+
+            byte offsetTransmitTime = 40;
+            ulong intpart = 0;
+            ulong fractpart = 0;
+
+            for (int i = 0; i <= 3; i++)
+                intpart = 256 * intpart + ntpData[offsetTransmitTime + i];
+
+            for (int i = 4; i <= 7; i++)
+                fractpart = 256 * fractpart + ntpData[offsetTransmitTime + i];
+
+            ulong milliseconds = (intpart * 1000 + (fractpart * 1000) / 0x100000000L);
+            s.Close();
+
+            TimeSpan timeSpan = TimeSpan.FromTicks((long)milliseconds * TimeSpan.TicksPerMillisecond);
+
+            DateTime dateTime = new DateTime(1900, 1, 1);
+            dateTime += timeSpan;
+
+            TimeSpan offsetAmount = TimeZone.CurrentTimeZone.GetUtcOffset(dateTime);
+            DateTime networkDateTime = (dateTime + offsetAmount);
+
+            return networkDateTime;
+        }
+    }
+}
