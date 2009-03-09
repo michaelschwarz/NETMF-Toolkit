011456968abf85e82823d9441b15ed64a565e4f4
interactive_cp <SND\interactive_cp>
2009-03-09 16:21:00 +0000

    set version to 9.3.9.1 (release)

--- NTP/Properties/AssemblyInfo.cs
+++ NTP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NTP")]
+[assembly: AssemblyTitle("NTP Library for Microsoft .NET Framework")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NTP")]
-[assembly: AssemblyCopyright("Copyright ©  2009")]
+[assembly: AssemblyCompany("Michael Schwarz")]
+[assembly: AssemblyProduct("NTP Library")]
+[assembly: AssemblyCopyright("Copyright ©  2009, Michael Schwarz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("9.3.9.1")]
+[assembly: AssemblyFileVersion("9.3.9.1")]
